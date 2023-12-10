1ecac7941a9f3fff58a9914cce66f00aecf6e5f1
Laukik verma <laukikverma03@gmail.com>
2023-12-10 08:58:19 +0530

    commit final

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -8,18 +8,6 @@ const Navbar = () => {
         data-te-navbar-ref>
         <div className="flex w-full flex-wrap items-center justify-between px-6">
           <div className="flex items-center">
-            {/* <button
-              className="block border-0 bg-transparent py-2 pr-2.5 text-neutral-500 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 lg:hidden"
-              type="button" data-te-collapse-init data-te-target="#navbarSupportedContentY"
-              aria-controls="navbarSupportedContentY" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="[&>svg]:w-7">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-7 w-7">
-                  <path fill-rule="evenodd"
-                    d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
-                    clip-rule="evenodd" />
-                </svg>
-              </span>
-            </button> */}
             <a
               className="mx-2 my-1 flex items-center text-neutral-900 hover:text-neutral-900 focus:text-neutral-900 lg:mb-0 lg:mt-0"
               href="#">
@@ -44,19 +32,9 @@ const Navbar = () => {
                 <a className="block py-2 pr-2 text-neutral-500 transition duration-150 ease-in-out hover:text-neutral-600 focus:text-neutral-600 disabled:text-black/30  dark:disabled:text-white/30 lg:px-2 [&.active]:text-black/80"
                   href="#!" data-te-nav-link-ref data-te-ripple-init data-te-ripple-color="light">About</a>
               </li>
-              <li className="mb-2 lg:mb-0" data-te-nav-item-ref>
-                <a className="block py-2 pr-2 text-neutral-500 transition duration-150 ease-in-out hover:text-neutral-600 focus:text-neutral-600 disabled:text-black/30  dark:disabled:text-white/30 lg:px-2 [&.active]:text-black/80"
-                  href="#!" data-te-nav-link-ref data-te-ripple-init data-te-ripple-color="light">CtaSection</a>
-              </li>
             </ul>
           </div>
-          {/* <div className="my-1 flex items-center lg:my-0 lg:ml-auto">
-            <button type="button"
-              className="mr-2 inline-block rounded px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-primary transition duration-150 ease-in-out hover:bg-neutral-500 hover:bg-opacity-10 hover:text-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:text-primary-700 dark:text-primary-400 dark:hover:bg-neutral-700 dark:hover:bg-opacity-60 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600"
-              data-te-ripple-init data-te-ripple-color="light">
-              Login
-            </button>
-          </div> */}
+         
         </div>
       </nav>
       <span className="[&>svg]:absolute [&>svg]:-z-10 [&>svg]:hidden [&>svg]:h-[560px] [&>svg]:w-full [&>svg]:lg:block">
@@ -95,7 +73,7 @@ const Navbar = () => {
               <h2 className="font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none max-w-lg mb-6">
                 Your Trusted Companinon 
                 <br className="hidden md:block" />
-                crypto wallet that’ll take {' '}
+                that’ll take {' '}
                 <span className="inline-block text-deep-purple-accent-400">you places</span>
               </h2>
               <p className="text-gray-700 text-base md:text-lg">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae. explicabo.</p>
